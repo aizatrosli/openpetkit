6094e3d5fcc9817367cd8b12240c604c65c6f2db
Ahmad Aizat <ahmadaizatr@gmail.com>
2022-02-04 15:56:01 +0800

    uidisplay

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,58 @@
 #include <Arduino.h>
+#include "SSD1306Wire.h"
+#include "OLEDDisplayUi.h"
+SSD1306Wire display(0x3c, D1, D2);
+OLEDDisplayUi ui     ( &display );
+#include "Pdisplay.h"
 
+FrameCallback frames[] = { drawFrame1, drawFrame2, drawFrame3, drawFrame4, drawFrame5 };
+int frameCount = 5;
+OverlayCallback overlays[] = { msOverlay };
+int overlaysCount = 1;
+
+void initUI() {
+  ui.setTargetFPS(60);
+
+  // Customize the active and inactive symbol
+  ui.setActiveSymbol(activeSymbol);
+  ui.setInactiveSymbol(inactiveSymbol);
+
+  // You can change this to
+  // TOP, LEFT, BOTTOM, RIGHT
+  ui.setIndicatorPosition(BOTTOM);
+
+  // Defines where the first frame is located in the bar.
+  ui.setIndicatorDirection(LEFT_RIGHT);
+
+  // You can change the transition that is used
+  // SLIDE_LEFT, SLIDE_RIGHT, SLIDE_UP, SLIDE_DOWN
+  ui.setFrameAnimation(SLIDE_LEFT);
+
+  // Add frames
+  ui.setFrames(frames, frameCount);
+
+  // Add overlays
+  ui.setOverlays(overlays, overlaysCount);
+
+  // Initialising the UI will init the display too.
+  ui.init();
+
+  display.flipScreenVertically();
+}
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  initUI();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int remainingTimeBudget = ui.update();
+
+  if (remainingTimeBudget > 0) {
+    // You can do some work here
+    // Don't do stuff if you are below your
+    // time budget.
+    delay(remainingTimeBudget);
+  }
 }
